bbb8beccd26d8d2fff8ab8eee06c9e357d9fc483
lzh11 <2239040999@qq.com>
2016-06-20 10:49:10 +0800

    L002VarAndContant
    通过视频学习，学会了简单的加减法，

---
+++ main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  L002Var and Contants
+//
+//  Created by LZH on 16/6/20.
+//  Copyright © 2016年 LZH. All rights reserved.
+//
+
+import Foundation
+
+var a = 1
+a = 5
+var b = 2
+b = 6
+let c = a+b
+print(c)
+
